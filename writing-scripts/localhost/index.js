e946877a614c23e53247f59cdaf1ce784650b099
blinkEgor <barminov44@gmail.com>
2023-10-06 11:28:35 +0300

    learn data storage

--- writing-scripts/localhost/index.js
+++ writing-scripts/localhost/index.js
@@ -6,7 +6,7 @@ const port = 4553;
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.sendFile(`${__dirname}/public/index.html`);
+    res.sendFile(`${__dirname}/public/webstorage.html`);
 });
 
 app.listen(port, () => {
